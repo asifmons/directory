8bdecd48abf501b3f49bd3117958119015a1e380
asifmons <asifshaji777@gmail.com>
2024-11-03 15:39:41 +0530

    third phase changes: implemented couple functionality

--- src/main/java/com/stjude/directory/model/Couple.java
+++ src/main/java/com/stjude/directory/model/Couple.java
@@ -1,10 +1,14 @@
 package com.stjude.directory.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Couple {
 
     private String spouce1;
