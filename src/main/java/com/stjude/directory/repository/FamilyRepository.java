69b3070ede6f8df1c5acc16882186a6f540f03c9
sijo.saji <sijosaji49@gmail.com>
2025-06-12 08:08:57 +0530

    Changes for photos -2

--- src/main/java/com/stjude/directory/repository/FamilyRepository.java
+++ src/main/java/com/stjude/directory/repository/FamilyRepository.java
@@ -4,8 +4,11 @@ import com.stjude.directory.model.Family;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface FamilyRepository extends MongoRepository<Family, String> {
+    Optional<Family> findByAathmaSthithiNumber(String athamsthithiNumber);
 }
 
